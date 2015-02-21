1f50071ba3e044746cc344e92adf8b0cd526fabd
Alfred Klomp <git@alfredklomp.com>
2015-02-21 16:58:56 +0000

    NEON encoder: use `vbsl` for tighter masking code

--- lib/enc/neon.c
+++ lib/enc/neon.c
@@ -3,14 +3,14 @@
  * do a full 16-byte read without segfaulting: */
 while (srclen >= 16)
 {
-	uint8x16_t res;
-	uint32x4_t str, mask;
+	uint8x16_t str, res;
+	uint32x4_t mask;
 	uint8x8_t tbl_shuf, lo, hi;
 	uint8_t t[8] = { 2, 2, 1, 0, 5, 5, 4, 3 };
 	uint8x16_t loset, hiset, himask;
 
 	/* Load string: */
-	str = vld1q_u32((uint32_t *)c);
+	str = vld1q_u8((uint8_t *)c);
 
 	/* Reorder to 32-bit big-endian.
 	 * The workset must be in big-endian, otherwise the shifted bits do not
@@ -44,28 +44,34 @@ while (srclen >= 16)
 	 *
 	 */
 	tbl_shuf = vld1_u8((uint8_t *)t);
-	lo = vtbl1_u8(vget_low_u8((uint8x16_t)str), tbl_shuf);
-	hi = vtbl1_u8(vget_high_u8(vextq_u8((uint8x16_t)str, (uint8x16_t)str, 14)), tbl_shuf);
-	str = (uint32x4_t)vcombine_u8(lo, hi);
+	lo = vtbl1_u8(vget_low_u8(str), tbl_shuf);
+	hi = vtbl1_u8(vget_high_u8(vextq_u8(str, str, 14)), tbl_shuf);
+	str = vcombine_u8(lo, hi);
 
 	/* Mask to pass through only the lower 6 bits of one byte: */
 	mask = vdupq_n_u32(0x3F000000);
 
 	/* Shift bits by 2, mask in only the first byte: */
-	res = (uint8x16_t)(vshrq_n_u32(str, 2) & mask);
-	mask = vshrq_n_u32(mask, 8);
+	res = (uint8x16_t)(vshrq_n_u32((uint32x4_t)str, 2) & mask);
 
 	/* Shift bits by 4, mask in only the second byte: */
-	res |= (uint8x16_t)(vshrq_n_u32(str, 4) & mask);
-	mask = vshrq_n_u32(mask, 8);
-
+	res = vbslq_u8(
+		(uint8x16_t)vshrq_n_u32(mask, 8),
+		(uint8x16_t)vshrq_n_u32((uint32x4_t)str, 4),
+		res
+	);
 	/* Shift bits by 6, mask in only the third byte: */
-	res |= (uint8x16_t)(vshrq_n_u32(str, 6) & mask);
-	mask = vshrq_n_u32(mask, 8);
-
+	res = vbslq_u8(
+		(uint8x16_t)vshrq_n_u32(mask, 16),
+		(uint8x16_t)vshrq_n_u32((uint32x4_t)str, 6),
+		res
+	);
 	/* Shift bits by 8, mask in only the fourth byte: */
-	res |= (uint8x16_t)(str & mask);
-
+	res = vbslq_u8(
+		(uint8x16_t)vshrq_n_u32(mask, 24),
+		str,
+		res
+	);
 	/* Reorder to 32-bit little-endian: */
 	res = vrev32q_u8(res);
 
@@ -88,11 +94,8 @@ while (srclen >= 16)
 		vtbl4_u8(tbl_enc_hi, vget_low_u8(hiset)),
 		vtbl4_u8(tbl_enc_hi, vget_high_u8(hiset))
 	);
-	/* Mask out unwanted results: */
-	hiset &= himask;
-
 	/* Combine and store result: */
-	vst1q_u8((uint8_t *)o, hiset | loset);
+	vst1q_u8((uint8_t *)o, vbslq_u8(himask, hiset, loset));
 
 	c += 12;	/* 3 * 4 bytes of input  */
 	o += 16;	/* 4 * 4 bytes of output */
