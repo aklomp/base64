011b20571b711ca5fd7dab1c522cde3fc78d381c
Alfred Klomp <git@alfredklomp.com>
2016-02-10 20:17:59 +0000

    Endian swap functions: define in codecs.h
    The NEON codecs use the uint_* codecs as fallback when the input is short, so
    the NEON codecs also need access to the endian-swapping functions. Place them
    in a shared header.

--- lib/codecs.h
+++ lib/codecs.h
@@ -56,6 +56,35 @@ struct codec
 	int  (* dec) BASE64_DEC_PARAMS;
 };
 
+// Define machine endianness. This is for GCC:
+#if (__BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__)
+	#define LITTLE_ENDIAN 1
+#else
+	#define LITTLE_ENDIAN 0
+#endif
+
+// This is for Clang:
+#ifdef __LITTLE_ENDIAN__
+	#define LITTLE_ENDIAN 1
+#endif
+
+#ifdef __BIG_ENDIAN__
+	#define LITTLE_ENDIAN 0
+#endif
+
+// Endian conversion functions
+#if LITTLE_ENDIAN
+	#define cpu_to_be32(x)	__builtin_bswap32(x)
+	#define cpu_to_be64(x)	__builtin_bswap64(x)
+	#define be32_to_cpu(x)	__builtin_bswap32(x)
+	#define be64_to_cpu(x)	__builtin_bswap64(x)
+#else
+	#define cpu_to_be32(x)	(x)
+	#define cpu_to_be64(x)	(x)
+	#define be32_to_cpu(x)	(x)
+	#define be64_to_cpu(x)	(x)
+#endif
+
 void codec_choose (struct codec *, int flags);
 
 // These tables are used by all codecs
