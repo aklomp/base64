fe0932b311fae4da9961225babd88d0bb45964bd
mayeut <mayeut@users.noreply.github.com>
2016-10-15 05:30:30 +0200

    Add SSE4.2 code paths
    SSE4.2 has been added for decoding.
    
    Speed-up on Intel(R) Core(TM) i7-4870HQ CPU @ 2.50GHz using Apple LLVM
    version 8.0.0 (clang-800.0.38)
    
    SSE4.2 decoding: +29% compared to SSSE3 in
    ab7a48bcbc8066e2e200836dd9f40a2b44dda35b

---
+++ lib/codec_sse42.c
@@ -0,0 +1,49 @@
+#include <stdint.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+#include "../include/libbase64.h"
+#include "codecs.h"
+
+#ifdef __SSE4_2__
+#include <nmmintrin.h>
+
+#define CMPGT(s,n)	_mm_cmpgt_epi8((s), _mm_set1_epi8(n))
+
+static inline __m128i
+dec_reshuffle (__m128i in)
+{
+	// Mask in a single byte per shift:
+	const __m128i maskB2 = _mm_set1_epi32(0x003F0000);
+	const __m128i maskB1 = _mm_set1_epi32(0x00003F00);
+
+	// Pack bytes together:
+	__m128i out = _mm_srli_epi32(in, 16);
+
+	out = _mm_or_si128(out, _mm_srli_epi32(_mm_and_si128(in, maskB2), 2));
+
+	out = _mm_or_si128(out, _mm_slli_epi32(_mm_and_si128(in, maskB1), 12));
+
+	out = _mm_or_si128(out, _mm_slli_epi32(in, 26));
+
+	// Reshuffle and repack into 12-byte output format:
+	return _mm_shuffle_epi8(out, _mm_setr_epi8(
+		 3,  2,  1,
+		 7,  6,  5,
+		11, 10,  9,
+		15, 14, 13,
+		-1, -1, -1, -1));
+}
+
+#endif	// __SSE42__
+
+BASE64_DEC_FUNCTION(sse42)
+{
+#ifdef __SSE4_2__
+	#include "dec/head.c"
+	#include "dec/sse42.c"
+	#include "dec/tail.c"
+#else
+	BASE64_DEC_STUB
+#endif
+}
