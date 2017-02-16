8a11f9275db2d24f1db6c8ea6b2774b1040cef82
Henrik S. Gaßmann <henrik@gassmann.onl>
2017-02-16 15:36:33 +0100

    CMAKE BUILDSYSTEM OVERHAUL
    Adapt the build system to the recent source code changes.
    
    Add platform detection code for arm, arm64, x86 and x64. Offer SIMD
    compile options depending on platform. Add preliminary support for NEON
    codecs. Enable the SIMD compile options by default.
    
    - Clean cmake scripts.
    - Bump version to 0.3.0.
    - Fix UTF-8 issues.

--- cmake/Modules/TargetArch.cmake
+++ cmake/Modules/TargetArch.cmake
@@ -1,4 +1,26 @@
+# Written in 2017 by Henrik Steffen Gaßmann henrik@gassmann.onl
+#
+# To the extent possible under law, the author(s) have dedicated all
+# copyright and related and neighboring rights to this software to the
+# public domain worldwide. This software is distributed without any warranty.
+#
+# You should have received a copy of the CC0 Public Domain Dedication
+# along with this software. If not, see
+#
+#     http://creativecommons.org/publicdomain/zero/1.0/
+#
+########################################################################
 
-macro(target_architecture OUTPUT_VARIABLE)
-    message(FATAL_ERROR "the target_architecture macro has not been implemented")
-endmacro()
+function(detect_target_architecture OUTPUT_VARIABLE)
+    try_compile(_IGNORED "${CMAKE_CURRENT_BINARY_DIR}"
+        "${CMAKE_SOURCE_DIR}/cmake/test-arch.c"
+        OUTPUT_VARIABLE _LOG
+    )
+
+    string(REGEX MATCH "##arch=([^#]+)##" _IGNORED "${_LOG}")
+
+    set(${OUTPUT_VARIABLE} "${CMAKE_MATCH_1}" PARENT_SCOPE)
+    if (CMAKE_MATCH_1 STREQUAL "unknown")
+        message(WARNING "could not detect the target architecture.")
+    endif()
+endfunction()
--- cmake/Modules/TargetSIMDInstructionSet.cmake
+++ cmake/Modules/TargetSIMDInstructionSet.cmake
@@ -1,4 +1,4 @@
-# Written in 2016 by Henrik Steffen Gaﬂmann henrik@gassmann.onl
+# Written in 2016-2017 by Henrik Steffen Ga√ümann henrik@gassmann.onl
 #
 # To the extent possible under law, the author(s) have dedicated all
 # copyright and related and neighboring rights to this software to the
@@ -11,76 +11,24 @@
 #
 ########################################################################
 
-include(TargetArch)
-include(CheckSymbolExists)
-
 ########################################################################
 # compiler flags definition
 macro(define_SIMD_compile_flags)
     if (CMAKE_C_COMPILER_ID STREQUAL "GNU" OR CMAKE_C_COMPILER_ID STREQUAL "Clang")
+        # x86
         set(COMPILE_FLAGS_SSSE3 "-mssse3")
+        set(COMPILE_FLAGS_SSE41 "-msse4.1")
+        set(COMPILE_FLAGS_SSE42 "-msse4.2")
+        set(COMPILE_FLAGS_AVX "-mavx")
         set(COMPILE_FLAGS_AVX2 "-mavx2")
-    #elseif(CMAKE_C_COMPILER_ID STREQUAL "MSVC") <- sorry about that, but old cmake versions think "MSVC" is a variable and must be dereferenced :(
+
+        #arm
+        set(COMPILE_FLAGS_NEON32 "-mfpu=neon")
     elseif(MSVC)
         set(COMPILE_FLAGS_SSSE3 " ")
+        set(COMPILE_FLAGS_SSE41 " ")
+        set(COMPILE_FLAGS_SSE42 " ")
+        set(COMPILE_FLAGS_AVX "/arch:AVX2")
         set(COMPILE_FLAGS_AVX2 "/arch:AVX2")
     endif()
 endmacro(define_SIMD_compile_flags)
-
-########################################################################
-# compiler feature detection (incomplete & currently unused)
-function(detect_target_SIMD_instruction_set_SSSE3 OUTPUT_VARIABLE)
-    define_SIMD_compile_flags()
-    
-    set(CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS} ${COMPILE_FLAGS_SSSE3}")
-    if (CMAKE_C_COMPILER_ID STREQUAL "GNU" OR CMAKE_C_COMPILER_ID STREQUAL "Clang")
-        check_symbol_exists(__SSSE3__ "tmmintrin.h" DTCTN_VALUE)
-    #elseif(CMAKE_C_COMPILER_ID STREQUAL "MSVC")
-    elseif(MSVC)
-        # with msvc one would have to try to compile a program referencing
-        # all used intrinsics. However I do know for sure that MSVC
-        # supports SSSE3 since MSVC 14 / VS2008...
-        if (MSVC_VERSION GREATER 1300)
-            set(DTCTN_VALUE 1)
-        else()
-            set(DTCTN_VALUE 0)
-        endif()
-    endif()
-    
-    if (DTCTN_VALUE)
-        set(${OUTPUT_VARIABLE} ${${OUTPUT_VARIABLE}} SSSE3-FOUND PARENT_SCOPE)
-    else()
-        set(${OUTPUT_VARIABLE} ${${OUTPUT_VARIABLE}} SSSE3-NOTFOUND PARENT_SCOPE)
-    endif()
-endfunction(detect_target_SIMD_instruction_set_SSSE3)
-
-function(detect_target_SIMD_instruction_set_AVX2 OUTPUT_VARIABLE)
-    define_SIMD_compile_flags()
-    
-    set(CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS} ${COMPILE_FLAGS_AVX2}")
-    check_symbol_exists(__AVX2__ "immintrin.h" DTCTN_VALUE)
-    
-    if (DTCTN_VALUE)
-        set(${OUTPUT_VARIABLE} ${${OUTPUT_VARIABLE}} AVX2-FOUND PARENT_SCOPE)
-    else()
-        set(${OUTPUT_VARIABLE} ${${OUTPUT_VARIABLE}} AVX2-NOTFOUND PARENT_SCOPE)
-    endif()
-endfunction(detect_target_SIMD_instruction_set_AVX2)
-
-function(detect_target_SIMD_instruction_set_NEON OUTPUT_VARIABLE)
-    
-endfunction(detect_target_SIMD_instruction_set_NEON)
-
-function(detect_target_SIMD_instruction_set OUTPUT_VARIABLE)
-    target_architecture(_TARGET_ARCH)
-    
-    if (APPLE AND CMAKE_OSX_ARCHITECTURES
-        OR _TARGET_ARCH STREQUAL "i386" 
-        OR _TARGET_ARCH STREQUAL "x86_64")
-        detect_target_SIMD_instruction_set_SSSE3(_TEMP_OUTPUT)
-        detect_target_SIMD_instruction_set_AVX2(_TEMP_OUTPUT)
-    elseif(_TARGET_ARCH MATCHES arm)
-        # add neon detection
-    endif()
-    set(${OUTPUT_VARIABLE} ${_TEMP_OUTPUT} PARENT_SCOPE)
-endfunction(detect_target_SIMD_instruction_set)
