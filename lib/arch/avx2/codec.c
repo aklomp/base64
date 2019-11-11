b6b186a64624797cbaec8bc820a2a66450a0f765
Alfred Klomp <git@alfredklomp.com>
2019-11-11 21:00:18 +0100

    Cleanup: AVX2: replace CMPGT macro with inline code
    Replace a macro used in one location with the actual code.

--- lib/arch/avx2/codec.c
+++ lib/arch/avx2/codec.c
@@ -8,8 +8,6 @@
 #if HAVE_AVX2
 #include <immintrin.h>
 
-#define CMPGT(s,n)	_mm256_cmpgt_epi8((s), _mm256_set1_epi8(n))
-
 static inline __m256i
 enc_reshuffle (const __m256i input)
 {
@@ -113,7 +111,7 @@ enc_translate (const __m256i in)
 	__m256i indices = _mm256_subs_epu8(in, _mm256_set1_epi8(51));
 
 	// mask is 0xFF (-1) for range #[1..4] and 0x00 for range #0:
-	__m256i mask = CMPGT(in, 25);
+	const __m256i mask = _mm256_cmpgt_epi8(in, _mm256_set1_epi8(25));
 
 	// substract -1, so add 1 to indices for range #[1..4], All indices are now correct:
 	indices = _mm256_sub_epi8(indices, mask);
