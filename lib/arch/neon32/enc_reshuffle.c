3d0e03f482c9711017b783e90f507fc95fca537d
Alfred Klomp <git@alfredklomp.com>
2019-11-28 23:32:40 +0000

    NEON32: enc_reshuffle: use faster algo and inline assembly
    Use a faster algorithm to do the reshuffling, and implement this
    algorithm in optimally pipelined inline assembly for extra gains.

--- lib/arch/neon32/enc_reshuffle.c
+++ lib/arch/neon32/enc_reshuffle.c
@@ -3,17 +3,52 @@ enc_reshuffle (const uint8x16x3_t in)
 {
 	uint8x16x4_t out;
 
-	// Divide bits of three input bytes over four output bytes:
-	out.val[0] = vshrq_n_u8(in.val[0], 2);
-	out.val[1] = vorrq_u8(vshrq_n_u8(in.val[1], 4), vshlq_n_u8(in.val[0], 4));
-	out.val[2] = vorrq_u8(vshrq_n_u8(in.val[2], 6), vshlq_n_u8(in.val[1], 2));
-	out.val[3] = in.val[2];
+#if defined(__GNUC__) || defined(__clang__)
 
-	// Clear top two bits:
-	out.val[0] = vandq_u8(out.val[0], vdupq_n_u8(0x3F));
-	out.val[1] = vandq_u8(out.val[1], vdupq_n_u8(0x3F));
-	out.val[2] = vandq_u8(out.val[2], vdupq_n_u8(0x3F));
-	out.val[3] = vandq_u8(out.val[3], vdupq_n_u8(0x3F));
+	// GCC and Clang support the following inline assembly syntax. This
+	// inline assembly implements the exact same algorithm as the
+	// intrinsics further down, but benchmarks show that the inline
+	// assembly easily beats the intrinsics. Perhaps this is because the
+	// inline assembly is well pipelined to avoid data dependencies.
+
+	__asm__ (
+		"vshr.u8 %q[o0], %q[i0], #2    \n\t"
+		"vshr.u8 %q[o1], %q[i1], #2    \n\t"
+		"vshr.u8 %q[o2], %q[i2], #4    \n\t"
+		"vsli.8  %q[o1], %q[i0], #6    \n\t"
+		"vsli.8  %q[o2], %q[i1], #4    \n\t"
+		"vshl.u8 %q[o3], %q[i2], #2    \n\t"
+
+		"vshr.u8 %q[o1], %q[o1], #2    \n\t"
+		"vshr.u8 %q[o2], %q[o2], #2    \n\t"
+		"vshr.u8 %q[o3], %q[o3], #2    \n\t"
+
+		// Outputs:
+		: [o0] "=&w" (out.val[0]),
+		  [o1] "=&w" (out.val[1]),
+		  [o2] "=&w" (out.val[2]),
+		  [o3] "=&w" (out.val[3])
+
+		// Inputs:
+		: [i0] "w" (in.val[0]),
+		  [i1] "w" (in.val[1]),
+		  [i2] "w" (in.val[2])
+	);
+#else
+	// Divide bits of three input bytes over four output bytes. All output
+	// bytes except the first one are shifted over two bits to the left:
+	out.val[0] = vshrq_n_u8(in.val[0], 2);
+	out.val[1] = vshrq_n_u8(in.val[1], 2);
+	out.val[2] = vshrq_n_u8(in.val[2], 4);
+	out.val[1] = vsliq_n_u8(out.val[1], in.val[0], 6);
+	out.val[2] = vsliq_n_u8(out.val[2], in.val[1], 4);
+	out.val[3] = vshlq_n_u8(in.val[2], 2);
+
+	// Clear the top two bits by shifting the output back to the right:
+	out.val[1] = vshrq_n_u8(out.val[1], 2);
+	out.val[2] = vshrq_n_u8(out.val[2], 2);
+	out.val[3] = vshrq_n_u8(out.val[3], 2);
+#endif
 
 	return out;
 }
