7770225d70d761c3390d42cb7601ab59284a78df
Alfred Klomp <git@alfredklomp.com>
2019-11-29 08:39:31 +0000

    Generic32: dec: unroll inner loop

--- lib/arch/generic/32/dec_loop.c
+++ lib/arch/generic/32/dec_loop.c
@@ -1,3 +1,34 @@
+static inline int
+dec_loop_generic_32_inner (const uint8_t **s, uint8_t **o, size_t *rounds)
+{
+	const uint32_t str
+		= base64_table_dec_d0[(*s)[0]]
+		| base64_table_dec_d1[(*s)[1]]
+		| base64_table_dec_d2[(*s)[2]]
+		| base64_table_dec_d3[(*s)[3]];
+
+#if BASE64_LITTLE_ENDIAN
+
+	// LUTs for little-endian set MSB in case of invalid character:
+	if (str & UINT32_C(0x80000000)) {
+		return 0;
+	}
+#else
+	// LUTs for big-endian set LSB in case of invalid character:
+	if (str & UINT32_C(1)) {
+		return 0;
+	}
+#endif
+	// Store the output:
+	memcpy(*o, &str, sizeof (str));
+
+	*s += 4;
+	*o += 3;
+	*rounds -= 1;
+
+	return 1;
+}
+
 static inline void
 dec_loop_generic_32 (const uint8_t **s, size_t *slen, uint8_t **o, size_t *olen)
 {
@@ -15,31 +46,39 @@ dec_loop_generic_32 (const uint8_t **s, size_t *slen, uint8_t **o, size_t *olen)
 	*olen += rounds * 3;	// 3 bytes produced per round
 
 	do {
-		const uint32_t str
-			= base64_table_dec_d0[(*s)[0]]
-			| base64_table_dec_d1[(*s)[1]]
-			| base64_table_dec_d2[(*s)[2]]
-			| base64_table_dec_d3[(*s)[3]];
-
-#if BASE64_LITTLE_ENDIAN
-
-		// LUTs for little-endian set MSB in case of invalid character:
-		if (str & UINT32_C(0x80000000)) {
+		if (rounds >= 8) {
+			if (dec_loop_generic_32_inner(s, o, &rounds) &&
+			    dec_loop_generic_32_inner(s, o, &rounds) &&
+			    dec_loop_generic_32_inner(s, o, &rounds) &&
+			    dec_loop_generic_32_inner(s, o, &rounds) &&
+			    dec_loop_generic_32_inner(s, o, &rounds) &&
+			    dec_loop_generic_32_inner(s, o, &rounds) &&
+			    dec_loop_generic_32_inner(s, o, &rounds) &&
+			    dec_loop_generic_32_inner(s, o, &rounds)) {
+				continue;
+			}
 			break;
 		}
-#else
-		// LUTs for big-endian set LSB in case of invalid character:
-		if (str & UINT32_C(1)) {
+		if (rounds >= 4) {
+			if (dec_loop_generic_32_inner(s, o, &rounds) &&
+			    dec_loop_generic_32_inner(s, o, &rounds) &&
+			    dec_loop_generic_32_inner(s, o, &rounds) &&
+			    dec_loop_generic_32_inner(s, o, &rounds)) {
+				continue;
+			}
 			break;
 		}
-#endif
-		// Store the output:
-		memcpy(*o, &str, sizeof (str));
+		if (rounds >= 2) {
+			if (dec_loop_generic_32_inner(s, o, &rounds) &&
+			    dec_loop_generic_32_inner(s, o, &rounds)) {
+				continue;
+			}
+			break;
+		}
+		dec_loop_generic_32_inner(s, o, &rounds);
+		break;
 
-		*s += 4;
-		*o += 3;
-
-	} while (--rounds > 0);
+	} while (rounds > 0);
 
 	// Adjust for any rounds that were skipped:
 	*slen += rounds * 4;
