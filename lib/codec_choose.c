3796b32cb92919c1435a158d1a4c6fac08c792b5
Alfred Klomp <git@alfredklomp.com>
2022-06-05 21:20:05 +0200

    codec_choose.c: x86 SSE4.2: factor out the runtime check

--- lib/codec_choose.c
+++ lib/codec_choose.c
@@ -138,6 +138,24 @@ x86_sse41_codec_runnable (const unsigned int max_level)
 #endif
 }
 
+// x86: SSE4.2: runtime check whether the codec is compiled in and compatible.
+static inline bool
+x86_sse42_codec_runnable (const unsigned int max_level)
+{
+	if (max_level < 1U) {
+		return false;
+	}
+
+#if defined (BASE64_X86_SIMD) && HAVE_SSE42
+	uint32_t eax, ebx, ecx, edx;
+
+	__cpuid(1, eax, ebx, ecx, edx);
+	return ecx & bit_SSE42;
+#else
+	return false;
+#endif
+}
+
 static bool
 codec_choose_forced (struct codec *codec, int flags)
 {
@@ -266,17 +284,11 @@ codec_choose_x86 (struct codec *codec)
 	}
 	#endif
 
-	#if HAVE_SSE42
-	// Check for SSE42 support:
-	if (max_level >= 1) {
-		__cpuid(1, eax, ebx, ecx, edx);
-		if (ecx & bit_SSE42) {
-			codec->enc = base64_stream_encode_sse42;
-			codec->dec = base64_stream_decode_sse42;
-			return true;
-		}
+	if (x86_sse42_codec_runnable(max_level)) {
+		codec->enc = base64_stream_encode_sse42;
+		codec->dec = base64_stream_decode_sse42;
+		return true;
 	}
-	#endif
 
 	if (x86_sse41_codec_runnable(max_level)) {
 		codec->enc = base64_stream_encode_sse41;
