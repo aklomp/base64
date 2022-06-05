bbc1bf22b687c304ec4f8c0a144857db0b4d1e17
Alfred Klomp <git@alfredklomp.com>
2022-06-05 22:52:56 +0200

    wip remove macos avx2 exclude

--- test/ci/test.sh
+++ test/ci/test.sh
@@ -7,10 +7,7 @@ if [ "${MACHINE}" == "x86_64" ]; then
 	export SSE41_CFLAGS=-msse4.1
 	export SSE42_CFLAGS=-msse4.2
 	export AVX_CFLAGS=-mavx
-	# no AVX2 on GHA macOS
-	if [ "$(uname -s)" != "Darwin" ]; then
-		export AVX2_CFLAGS=-mavx2
-	fi
+	export AVX2_CFLAGS=-mavx2
 elif [ "${MACHINE}" == "aarch64" ]; then
 	export NEON64_CFLAGS="-march=armv8-a"
 elif [ "${MACHINE}" == "armv7l" ]; then
